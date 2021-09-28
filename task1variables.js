fe8c6a09699681926a2ac644575b45fba8fd74b4
Manoradha7 <91074732+Manoradha7@users.noreply.github.com>
2021-09-28 07:40:56 +0530

    Create task1variables.js

---
+++ task1variables.js
@@ -0,0 +1,47 @@
+// 1.Declare four variables without assigning values and print them in console
+var a,b,c,d;
+console.log(a,b,c,d);
+
+
+// 2.How to get value of the variable myvar as output
+var myvar= 1;
+console.log(myvar);
+
+// 3.Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+var first_name= "Mano";
+var last_name= "Radha";
+var martial_status = "Noo";
+var country ="India";
+var age= 21;
+
+// 4.Declare variables to store your first name, last name, marital status, country and age in a single line
+
+var first_name ="Mano",last_name= "Radha", martial_status="Noo",country="India",age=21;
+
+// 5.Declare variables and assign string, boolean, undefined and null data types
+
+var str = "I am Mano";
+var boolean = true;
+var mano = [];
+var value = null;
+
+// 6.Convert the string to integer
+
+var str= "263879"
+console.log(parseInt(str));
+console.log(Number(str));
+console.log(+(str));
+
+// 7.Write 6 statement which provide truthy & falsey values.
+
+var a = "undefined";  //truthy
+var b = 123;  //truthy
+var c =  undefined; //falshy
+var d = null;  //falshy
+var e = 0 ; //falshy
+var f = "" //falshy
+var g = true;  //truthy
+var h = NaN ; //falshy
+
+
