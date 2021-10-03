ff27bc2887ee2089a37db5a87a6856ac331929ba
Manoradha7 <91074732+Manoradha7@users.noreply.github.com>
2021-10-03 07:32:17 +0530

    Create XHR.js

---
+++ XHR.js
@@ -0,0 +1,16 @@
+var XHR = new XMLHttpRequest();
+
+XHR.open('GET', 'https://restcountries.com/v3/all');
+XHR.responseType="json"
+
+XHR.onload = function(){
+    const data=XHR.response;
+    // console.log(data)
+    for(let i in data){
+        console.log(data[i].name.common)
+        console.log(data[i].region)
+        console.log(data[i].subregion)
+          console.log(data[i].population)
+    }
+}
+XHR.send();
